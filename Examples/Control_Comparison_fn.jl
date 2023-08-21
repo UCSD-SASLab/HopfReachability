13bbb7457638b5360f7ea2ffe28331d69f30b806
willsharpless <52509586+willsharpless@users.noreply.github.com>
2023-08-20 18:52:55 -0700

    fixed a few update bugs

--- Examples/Control_Comparison_fn.jl
+++ Examples/Control_Comparison_fn.jl
@@ -4,7 +4,7 @@
 #   wsharpless@ucsd.edu
 #   ---------------------
 
-using LinearAlgebra, Plots
+using LinearAlgebra, Plots, Suppressor, LaTeXStrings, TickTock
 plotlyjs()
 
 ## Comparison
@@ -13,30 +13,41 @@ using iLQGames, JuMP, Gurobi
 #   Utility Fn
 #   ==========
 
-function roll_out(system, target, ctrls, x0, steps; random_d=false, random_same=true, worst_d=false, surface=false, printing=false, plotting=false, title="Controlled Trajectories")
+function LinearEuler(x, u, d; p=(I, I, I), th=5e-2)
+    return (I + th * p[1]) * x + (th * p[2]) * u + (th * p[3]) * d
+end
+
+function roll_out(system, target, ctrls, x0, steps; F = LinearEuler, ψ = x -> x,
+                    nx = size(system[1])[2], nu = size(system[2])[2], nd = size(system[3])[2], th = 5e-2,
+                    random_d=false, random_same=true, same_d=nothing, surface=false, printing=false, 
+                    plotting=false, title="Controlled Trajectories")
+
     """
     Function for rolling out a dictionary of controllers from a given initial condition.
+        nx, nu, nd  : state, control and disturbance dimensions
+        th          : time interval for evolution (implicitly, control and disturbance constant during interval)
+        F           : function for evolving the state (default linear Euler)
+        ψ           : function for lifting the state (default id)
         random_d    : apply random disturbance to all
         random_same : apply same random disturbance (if random)
         surface     : generates random disturbance on surface of constraint
-        worst_d     : apply the disturbance from the "Hopf" controller to all
+        same_d      : apply the same disturbance from the specified controller
     
         e.g.
 
-        ctrls = Dict("Hopf"=> Hopf_minT(intH=intH_ytc17, HJoc=HJoc_ytc17, preH=preH_ytc17, time_p = (0.05, 0.4, 2.0)),
-                     "MPCs"=> MPC_stochastic(H=10, N_T=20),
-                     "MPCg"=> MPC_game(H=1, its=1))
+        ctrls = Dict("Hopf" => x -> Hopf_minT(system, target, x; time_p = (0.05, 0.4, 2.0)),
+                     "MPCs" => x -> MPC_stochastic(system, target, x; H=10, N_T=20),
+                     "MPCg" => x -> MPC_game(system, target, x; H=1, its=1));
+        
+        Xs, Us, Ds = roll_out(system, target, ctrls, x0, steps)
     """
 
     ## Initialize
-    M, C, C2, Q, Q2, a, a2 = system
-    Md, Cd, C2d = diagm(ones(size(M)[1])) + th * M, th * C, th * C2;
-    Tˢ, dϕdz = 0, 0 #for plot_game
-
-    ## Store Trajectories
     Xs = Dict(key => hcat(x0, zeros(nx, steps)) for key in keys(ctrls));
-    Us = Dict(key => zeros(nx, steps) for key in keys(ctrls));
-    Ds = Dict(key => zeros(nx, steps) for key in keys(ctrls)); 
+    Us = Dict(key => zeros(nu, steps) for key in keys(ctrls));
+    Ds = Dict(key => zeros(nd, steps) for key in keys(ctrls)); 
+    Ts = Dict(key => zeros(steps) for key in keys(ctrls)); 
+    Tˢ, dϕdz = 0, 0 #for plot_game
     
     ## Iterate through time
     for s in 1:steps
@@ -44,44 +55,127 @@ function roll_out(system, target, ctrls, x0, steps; random_d=false, random_same=
 
         ## Compute Feedback
         for c in keys(ctrls)
-            outputs = ctrls[c](Xs[c][:, s])
+
+            @suppress begin; tick(); end # timing
+
+            if startswith(c, L"Hopf")
+                outputs = ctrls[c](ψ(Xs[c][:, s]))
+                us, ds, Tˢ, ϕ, dϕdz = outputs;
+                if printing; println("$c will reach in $Tˢ s (ϕ = $ϕ)"); end
+            else
+                try
+                    @suppress outputs = ctrls[c](ψ(Xs[c][:, s]))
+                catch;
+                    outputs = (Us[c][:, s-1], Ds[c][:, s-1]) # rare occurence when primal oob
+                end
+            end
+
+            Ts[c][s] = tok()
+
             Us[c][:, s], Ds[c][:, s] = outputs[1], outputs[2]
-            if plotting && c == "Hopf"; Tˢ, dϕdz = outputs[3], outputs[5]; end
         end
 
         ## Choose Disturbance
-        if worst_d
-            if printing; print(" with worst disturbance"); end
-            for c in keys(ctrls); Ds[c][:, s] = Ds["Hopf"][:, s]; end
+        if !isnothing(same_d)
+            if printing; print(" perturbed by the $same_d disturbance"); end
+            for c in keys(ctrls); Ds[c][:, s] = Ds[same_d][:, s]; end
         elseif random_d 
-            if printing; print(" with random disturbance"); end
-            d = ellipsoid_sample(1, Q2, 1.; surface)
+            if printing; print(" perturbed by random disturbance"); end
+            d = ellipsoid_sample(1, system[5], 1.; surface)
             for c in keys(ctrls); Ds[c][:, s] = d; end
         else
-            if printing; print(" with their estimated worst disturbance"); end
+            if printing; print(" perturbed by their own estimated worst disturbance"); end
         end
 
         ## Evolve States
         for c in keys(ctrls)
-            if random_d && !random_same; Ds[c][:, s] = ellipsoid_sample(1, Q2, 1., surface); if printing; print(" (different)"); end; end
-            Xs[c][:, s + 1] = Md * Xs[c][:, s] + Cd * Us[c][:, s] + C2d * Ds[c][:, s]
+            if random_d && !random_same; Ds[c][:, s] = ellipsoid_sample(1, system[5], 1.; surface); if printing; print(" (different)"); end; end
+            Xs[c][:, s+1] = F(Xs[c][:, s], Us[c][:, s], Ds[c][:, s]; p=system, th)
         end
 
         ## Plot Game at First Step
         if s == 1 && plotting
+            _, _, Tˢ, _, dϕdz = Hopf_minT(system, target, x0; time_p = (0.05, 0.4, 2.0))
             pair_strats = [(Us[c][:, s], Ds[c][:, s]) for c in keys(ctrls)]; pair_labels = collect(keys(ctrls))
-            game_plot = plot_game(system, pair_strats, pair_labels; p=dϕdz, t=Tˢ)
+            game_plot = plot_game(system, pair_strats, pair_labels; p=dϕdz, t=Tˢ); display(game_plot);
         end
     end
 
     if plotting; sim_plot = plot_sim(system, target, Xs; title); display(sim_plot); end
 
-    return Xs, Us, Ds
+    return Xs, Us, Ds, Ts
 end
+
+
+# function roll_out(system, target, ctrls, x0, steps; random_d=false, random_same=true, worst_d=false, surface=false, printing=false, plotting=false, title="Controlled Trajectories")
+#     """
+#     Function for rolling out a dictionary of controllers from a given initial condition.
+#         random_d    : apply random disturbance to all
+#         random_same : apply same random disturbance (if random)
+#         surface     : generates random disturbance on surface of constraint
+#         worst_d     : apply the disturbance from the "Hopf" controller to all
+    
+#         e.g.
+
+#         ctrls = Dict("Hopf" => Hopf_minT(intH=intH_ytc17, HJoc=HJoc_ytc17, preH=preH_ytc17, time_p = (0.05, 0.4, 2.0)),
+#                      "MPCs" => MPC_stochastic(H=10, N_T=20),
+#                      "MPCg" => MPC_game(H=1, its=1))
+#     """
+
+#     ## Initialize
+#     M, C, C2, Q, Q2, a, a2 = system
+#     Md, Cd, C2d = diagm(ones(size(M)[1])) + th * M, th * C, th * C2;
+#     Tˢ, dϕdz = 0, 0 #for plot_game
+
+#     ## Store Trajectories
+#     Xs = Dict(key => hcat(x0, zeros(nx, steps)) for key in keys(ctrls));
+#     Us = Dict(key => zeros(nx, steps) for key in keys(ctrls));
+#     Ds = Dict(key => zeros(nx, steps) for key in keys(ctrls)); 
+    
+#     ## Iterate through time
+#     for s in 1:steps
+#         if printing; println(" on step $s ..."); end
+
+#         ## Compute Feedback
+#         for c in keys(ctrls)
+#             outputs = ctrls[c](Xs[c][:, s])
+#             Us[c][:, s], Ds[c][:, s] = outputs[1], outputs[2]
+#             if plotting && c == "Hopf"; Tˢ, dϕdz = outputs[3], outputs[5]; end
+#         end
+
+#         ## Choose Disturbance
+#         if worst_d
+#             if printing; print(" with worst disturbance"); end
+#             for c in keys(ctrls); Ds[c][:, s] = Ds["Hopf"][:, s]; end
+#         elseif random_d 
+#             if printing; print(" with random disturbance"); end
+#             d = ellipsoid_sample(1, Q2, 1.; surface)
+#             for c in keys(ctrls); Ds[c][:, s] = d; end
+#         else
+#             if printing; print(" with their estimated worst disturbance"); end
+#         end
+
+#         ## Evolve States
+#         for c in keys(ctrls)
+#             if random_d && !random_same; Ds[c][:, s] = ellipsoid_sample(1, Q2, 1.; surface); if printing; print(" (different)"); end; end
+#             Xs[c][:, s + 1] = Md * Xs[c][:, s] + Cd * Us[c][:, s] + C2d * Ds[c][:, s]
+#         end
+
+#         ## Plot Game at First Step
+#         if s == 1 && plotting
+#             pair_strats = [(Us[c][:, s], Ds[c][:, s]) for c in keys(ctrls)]; pair_labels = collect(keys(ctrls))
+#             game_plot = plot_game(system, pair_strats, pair_labels; p=dϕdz, t=Tˢ)
+#         end
+#     end
+
+#     if plotting; sim_plot = plot_sim(system, target, Xs; title); display(sim_plot); end
+
+#     return Xs, Us, Ds
+# end
 
 function plot_sim(system, target, Xs; grid_p=((-3, 3), 10.), title="Controlled Trajectories", plot_drift=true, plot_target=true, scale=0.6)
     """
-    Plots the Controlled Trajectories, Target and Autonomous Drift from a dictionary of paths.
+    Plots the Controlled Trajectories, Target and Autonomous Drift from a dictionary of paths. (for 2D)
     """
 
     M, C, C2, Q, Q2, a, a2 = system
@@ -110,6 +204,70 @@ function plot_sim(system, target, Xs; grid_p=((-3, 3), 10.), title="Controlled T
     return sim_plot
 end
 
+function plot_sim_sbs(Xs; th, steps=size(first(Xs)[2])[2]-1, nx=size(first(Xs)[2])[1], 
+    labels=[L"\text{Glucose}" L"\text{Phosphate Pool}" L"1,3-\text{BPG}" L"\text{Pyruvate} \:\: \& \:\: \text{Acetate}" L"\text{NADH}" L"\text{ATP}" L"\text{Glucose}\:\: \text{Ext} \:\: (\leftarrow u_1)" L"\text{Pyruvate} \:\: \& \:\: \text{Acetate} \:\: \text{Ext}" L"\text{NAD}^+ / \text{NADH}\:\:\text{Pool}  \:\: (\leftarrow u_2) " L"\text{ATP}/\text{ADP}\:\:\text{Pool}  \:\: (\leftarrow u_3)"], 
+    dim_splits=[[1,2,3,4,5,6,8], [7,9,10]], pal=:seaborn_colorblind, semilog=false,)
+
+    sim_plots = []
+    for (ci, c) in enumerate(sort(collect(keys(ctrls))))
+        labels = ci == 1 ? labels : reshape(["" for _=1:nx], 1, nx)
+
+        split_plots = []
+        for (ixsi, ixs) in enumerate(dim_splits)
+            split_plot = plot(th:th:th*(steps+1), Xs[c]'[:,ixs], 
+                            label=labels[:, ixs], linewidth=3, alpha=0.8, yscale = semilog ? :log10 : :identity,
+                            palette = palette(pal)[[end:-1:1, 1:end][ixsi]])
+            
+            if ixsi == 1;
+                hline!([2.5], linestyle=:dash, color=:black, alpha=0.4, linewidth=2, label= ci == 1 ? L"\text{Target}\:\:\text{ATP}" : "")
+            end
+
+            if ci == 1;
+                if ixsi == 1; 
+                    plot!(ylims=[-0.01, 3.01], yticks = ([0,1,2,3], [L"0", L"1", L"2", L"3"])); 
+                else
+                    plot!(ylims=[-0.01, 5.02], yticks = ([0,1,2,3,4,5], [L"0", L"1", L"2", L"3", L"4", L"5"]));
+                end
+            else                
+                if ixsi == 1; 
+                    plot!(ylims=[-0.01, 3.01], yticks = ([0,1,2,3], ["", "", "", ""])); 
+                else
+                    plot!(ylims=[-0.01, 5.02], yticks = ([0,1,2,3,4,5], ["", "", "", "", "", ""]));
+                end
+            end
+
+            if ixsi == 2; 
+                plot!(xlims=[-0.01, 3.01], xticks = ([1,2,3], [L"1", L"2", L"3"]));
+                # if ci == 1; plot!(ylabel = L"x_i", xlabel = L"t"); end 
+            else
+                plot!(xlims=[-0.01, 3.01], xticks = ([0,1,2,3],["", "", "", ""]));
+            end
+
+            push!(split_plots, split_plot)
+        end
+
+        sim_plot_c = plot(split_plots..., layout=(length(dim_splits),1), legend=:outertopright, plot_title=LaTeXString(c))
+
+        push!(sim_plots, sim_plot_c)
+    end
+    
+    sim_plot = plot(sim_plots..., layout=(1, length(keys(Xs))), legend=:outertopright, size=(900, 500))
+
+    YLABEL = plot(title = L"\: \\ \: \\ x_i \\ \: \\ \: \\ \: \\ \: \\ \: \\ \: \\  \: \\ x_i", fontsize=10, grid = false, showaxis = false, right_margin=-50Plots.px) #, left_margin = 10Plots.px)    
+    
+    # empty = plot(title = L"", grid = false, showaxis = false, bottom_margin = -50Plots.px)
+    XLABEL = plot(title = L"\: \\ \text{Time (s)}", grid = false, showaxis = false, bottom_margin = -50Plots.px)
+    
+    # XLABEL = plot(empty, XLABEL, layout= @layout[a{0.01h}; b])
+
+    sim_plot = plot(sim_plot, XLABEL, layout= @layout[a; b{0.01h}], size=(900, 500), legend=:outertopright)
+    sim_plot = plot(YLABEL, sim_plot, layout=@layout[a{0.01w}  b], size=(900, 500), legend=:outertopright)
+
+    plot!(sim_plot, extra_plot_kwargs = KW(:include_mathjax => "cdn"), legend=false)
+
+    return sim_plot
+end
+
 
 function MPC_game(system, target, x0; th=5e-2, H=1, its=1, input_constraint="coupled", solver=Gurobi.Optimizer)
     """
@@ -120,6 +278,7 @@ function MPC_game(system, target, x0; th=5e-2, H=1, its=1, input_constraint="cou
     M, C, C2, Q, Q2, a, a2 = system
     Md, Cd, C2d = diagm(ones(size(M)[1])) + th * M, th * C, th * C2;
     J, _, Jp = target
+    Ap, cp = Jp
 
     nx, nu, nd = size(M)[1], size(C)[2], size(C2)[2]
     uˢH, dˢH = zeros(nu, H), zeros(nu, H)
@@ -170,9 +329,9 @@ function MPC_game(system, target, x0; th=5e-2, H=1, its=1, input_constraint="cou
             end
 
             if player == "d"
-                @objective(model, Max, 0.5 * (x[:, H+1]' * inv(Ap) * x[:, H+1] - 1))
+                @objective(model, Max, 0.5 * ((x[:, H+1] .- cp)' * inv(Ap) * (x[:, H+1] .- cp) - 1))
             elseif player == "u"
-                @objective(model, Min, 0.5 * (x[:, H+1]' * inv(Ap) * x[:, H+1] - 1))
+                @objective(model, Min, 0.5 * ((x[:, H+1] .- cp)' * inv(Ap) * (x[:, H+1] .- cp) - 1))
             end
             optimize!(model)
 
@@ -228,7 +387,7 @@ function MPC_stochastic(system, target, x0; th=5e-2, H=3, N_T=10, input_constrai
         end
     end
 
-    @objective(model, Min, sum(0.5 * (x[:, H+1, k]' * inv(Ap) * x[:, H+1, k] - 1) for k in 1:N_T)/k);
+    @objective(model, Min, sum(0.5 * ((x[:, H+1, k] .- cp)' * inv(Ap) * (x[:, H+1, k] .- cp) - 1) for k in 1:N_T)/k);
     optimize!(model);
 
     uˢH = value.(u)
--- Examples/HopfReachability_demo.jl
+++ Examples/HopfReachability_demo.jl
@@ -2,27 +2,27 @@
 using LinearAlgebra, Plots
 plotlyjs()
 include("/Users/willsharpless/Library/Mobile Documents/com~apple~CloudDocs/Herbert/Koop_HJR/HL_fastHJR/HopfReachability.jl");
-using .HopfReachability: Hopf_BRS, Hopf_admm, Hopf_cd, intH_ytc17, preH_ytc17, plot_BRS, Hopf
+using .HopfReachability: Hopf_BRS, Hopf_admm_cd, Hopf_admm, Hopf_cd, intH_ytc17, preH_ytc17, plot_BRS, Hopf
 
 ## Initialize (2D Example)
-M = [0. 1; -2 -3]
-B = 0.5 * [1 0; 0 1]
-C = 0.5 * [2 0; 0 1]
-Q = 0.1 * 3 * [1 0; 0 1]
-Q2 = 0.2 * 2 * [1 0; 0 1]
-a1 = 0*[0.5 0.75]
-a2 = -0*[0.5 0]
-system = (M, B, C, Q, Q2, a1, a2)
+A = [0. 1; -2 -3]
+B₁ = 0.5 * [1 0; 0 1]
+B₂ = 0.5 * [2 0; 0 1]
+Q₁ = 0.1 * 3 * [1 0; 0 1]
+Q₂ = 0.2 * 2 * [1 0; 0 1]
+c₁ = 0*[0.5 0.75]
+c₂ = -0*[0.5 0]
+system = (A, B₁, B₂, Q₁, Q₂, c₁, c₂)
 
-## Initialize (3D Example)
-# M = [0. 1 0.; -2 -3 0.; 0. 0. -1.]
-# B = 0.5 * [1 0; 0 1; 0. 0.]
-# C = 0.5 * [2 0; 0 1; 0. 0.]
-# Q = 0.1 * 3 * [1 0; 0 1]
-# Q2 = 0.2 * 2 * [1 0; 0 1]
-# a1 = 0*[0.5 0.75]
-# a2 = -0*[0.5 0]
-# system = (M, B, C, Q, Q2, a1, a2)
+## Initialize (uncommet for 3D Example)
+# A = [0. 1 0.; -2 -3 0.; 0. 0. -1.]
+# B₁ = 0.5 * [1 0; 0 1; 0. 0.]
+# B₂ = 0.5 * [2 0; 0 1; 0. 0.]
+# Q₁ = 0.1 * 3 * [1 0; 0 1]
+# Q₂ = 0.2 * 2 * [1 0; 0 1]
+# c₁ = 0*[0.5 0.75]
+# c₂ = -0*[0.5 0]
+# system = (A, B₁, B₂, Q₁, Q₂, c₁, c₂)
 
 ## Time
 th = 0.02
@@ -31,64 +31,43 @@ Tf = 0.8
 T = collect(Th : Th : Tf)
 
 ## Target: J(x) = 0 is the boundary of the target
-Ap = diagm([1.5, 1]) # Ap = diagm([1.5, 1, 1.])
-cp = [0.; 0.] # cp = [0.; 0.; 0.]
-r = 2.0
-J(x::Vector, A, c) = ((x - c)' * inv(A) * (x - c))/2 - 0.5 * r^2 #don't need yet
-Js(v::Vector, A, c) = (v' * A * v)/2 + c'v + 0.5 * r^2
-J(x::Matrix, A, c) = diag((x .- c)' * inv(A) * (x .- c))/2 .- 0.5 * r^2
-Js(v::Matrix, A, c) = diag(v' * A * v)/2 + (c'v)' .+ 0.5 * r^2 #don't need yet
-target = (J, Js, (Ap, cp))
+Qₓ = diagm(vcat([1.5], ones(size(A)[1]-1)))
+cₓ = zero(A[:,1])
+r = 1.0
+J(x::Vector, Qₓ, cₓ) = ((x - cₓ)' * inv(Qₓ) * (x - cₓ))/2 - 0.5 * r^2 #don't need yet
+Jˢ(v::Vector, Qₓ, cₓ) = (v' * Qₓ * v)/2 + cₓ'v + 0.5 * r^2
+J(x::Matrix, Qₓ, cₓ) = diag((x .- cₓ)' * inv(Qₓ) * (x .- cₓ))/2 .- 0.5 * r^2
+Jˢ(v::Matrix, Qₓ, cₓ) = diag(v' * Qₓ * v)/2 + (cₓ'v)' .+ 0.5 * r^2 #don't need yet
+target = (J, Jˢ, (Qₓ, cₓ))
 
-## Point Target (Indicator Set)
-# ap = -1.
-# cp = [0.; 0.]
-# J(x::Vector, a, c) = x == c ? a : Inf
-# Js(v::Vector, a, c) = v'c - a
-# J(x::Matrix, a, c) = map(x -> x == c ? a : Inf, eachcol(x))
-# Js(v::Matrix, a, c) = v'c .- a
-# target = (J, Js, (ap, cp))
-
-## Affine Target
-# ap = [1.; 1.]
-# cp = 0.
-# J(x::Vector, a, c) = a'x - c #don't need yet
-# Js(v::Vector, a, c) = v == a ? c : 10
-# J(x::Matrix, a, c) = (a'x)' .- c
-# Js(v::Matrix, a, c) = map(v -> v == a ? c : 10, eachcol(v))
-# target = (J, Js, (ap, cp))
-
-## Grid Parameters
-bd = 3. # (-3, 3) for ellipse
+## Automatic Grid Parameters (can also define matrix of points Xg)
+bd = 4
 ϵ = 0.5e-7
 N = 3 + ϵ
 grid_p = (bd, N)
 
-## Hopf ADMM Parameters (default)
-ρ, ρ2 = 1e-4, 1e-4
-tol = 1e-5
-max_its = 10
-opt_p_admm = (ρ, ρ2, tol, max_its)
-
-## Hopf CD Parameters (default)
+## Hopf Coordinate-Descent Parameters (optional)
 vh = 0.01
 L = 5
 tol = ϵ
 lim = 500
-lll = 20
-max_runs = 40
-opt_p_cd = (vh, L, tol, lim, lll, max_runs)
+lll = 5
+max_runs = 3
+max_its = 500
+opt_p_cd = (vh, L, tol, lim, lll, max_runs, max_its)
 
-solution, run_stats = Hopf_BRS(system, target, intH_ytc17, T;
-                                                    opt_method=Hopf_cd,
-                                                    preH=preH_ytc17,
-                                                    th,
-                                                    grid_p,
-                                                    opt_p=opt_p_cd,
-                                                    warm=false,
-                                                    check_all=true,
-                                                    printing=true);
-B⁺T, ϕB⁺T = solution;
+# Hopf ADMM Parameters (optional)
+ρ, ρ2 = 1e-1, 1e-1
+tol = 1e-5
+max_its = 3
+opt_p_admm = (ρ, ρ2, tol, max_its)
 
-plot_scatter = plot_BRS(T, B⁺T, ϕB⁺T; M, ϵs=0.1, interpolate=false, value_fn=true, alpha=0.5)
-plot_contour = plot_BRS(T, B⁺T, ϕB⁺T; M, ϵc=0.01, interpolate=true, value_fn=true, alpha=0.5)
+# ADMM-CD Hybrid Parameters (optional)
+ρ_grid_vals = 1 
+hybrid_runs = 3
+opt_p_admm_cd = ((1e-1, 1e-1, 1e-5, 3), (0.005, 5, 1e-4, 500, 1, 3, 500), ρ_grid_vals, ρ_grid_vals, hybrid_runs)
+
+solution, run_stats = Hopf_BRS(system, target, T; th, grid_p, opt_method=Hopf_cd, opt_p=opt_p_cd, warm=true, check_all=true, printing=true);
+
+plot_scatter = plot_BRS(T, solution...; A, ϵs=2e-1, interpolate=false, value_fn=true, alpha=0.1)
+plot_contour = plot_BRS(T, solution...; A, ϵc=1e-3, interpolate=true, value_fn=true, alpha=0.5)
